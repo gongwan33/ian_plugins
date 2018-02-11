7446b76c1198a30c33c78ae9ad7a3ae3e042d3d9
gong <gongwan33@gmail.com>
2018-02-11 21:50:06 +0800

    add spider

--- super-import/run_ozdazhe.sh
+++ super-import/run_ozdazhe.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 echo "Start Executing..."
 cd $1'/spider_ozdazhe'
-echo "Enter into spider..."
+echo "Enter into ozdazhe spider..."
 pwd
 scrapy crawl ozdazhe-com 
 echo "Finished."
+
+echo "Start Executing..."
+cd $1'/deamoon_spider'
+echo "Enter into dealmoon spider..."
+pwd
+scrapy crawl deamoon 
+echo "Finished."
+
